fa3e467101395b384655b9afcfed7da0ff160b2f
Sohail <sashaikh@ncsu.edu>
2019-03-21 16:50:26 -0400

    #27 Initial framework
    Changes:
    RequestParser: Changed it to be a generic handler for requests.
    RequestResponseHandler: Generic framework for different apis we suppot.
    WorkloadPredictionHandler: Structure of interface to prediction algos.
    
    Workflow:
    Different apis that we support have different handlers identified
    by their apiName and inheriting RequestResponseHandler.
    These have to be registered with RequestParser.
    When the parser parses a request, it hands it off to a handler
    based on the apiName.
    
    Testing:
    This is untested right now

--- predictor/src/RequestParser.py
+++ predictor/src/RequestParser.py
@@ -1,4 +1,23 @@
-# interact with the scheduler and parse requests
 class RequestParser:
+	handlers_ = []
 	def __init__(self):
 		pass
+
+	# request is a JSon object
+	def parse(request):
+		# extract apiName and determine which request handler to invoke
+		# right now we will have only one request handler
+		# apiName = extract api name from request
+		for i, handler in enumerate(handlers_):
+			if (handler.apiName_ == apiName):
+				# encode a response
+				handler.process()
+				handler.encode()
+				break
+
+	# register different handlers
+	def registerHandler(handler):
+		handlers_.append(obj);
+		pass
+
+
---
+++ predictor/src/RequestResponseHandler.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+class RequestResponseHandler:
+	def __init__(self, apiName):
+		apiName_ = apiName
+	apiName_ = ""
+	# encode a json object and return it
+
+	def encode():
+		pass
+
+class WorkloadPredictionHandler(RequestResponseHandler):
+	def __init__(self, apiName):
+		super().__init__(apiName)
+
+	def encode():
+		pass
