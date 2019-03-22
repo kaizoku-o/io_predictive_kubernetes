6e9fdf8bfc0897bbae88bc9d679a6db203d51ea2
vbongale <vabongal@ncsu.edu>
2019-03-22 18:21:13 -0400

    Integrate prometheus handler to collect metrics

--- predictor/src/PrometheusDataHandler.py
+++ predictor/src/PrometheusDataHandler.py
@@ -1,9 +1,10 @@
 from BaseDataHandler import BaseDataHandler
+from prometheusQueries import Queries
+from prometheusUtils import run_query_range
 
 class PrometheusDataHandler(BaseDataHandler):
 	def __init__(self):
 		super().__init__()
 
 	def get_data():
-		# return csv values
-		pass
+		return run_query_range(Queries["cpu"], 1552290000, 1552320000, 60)
---
+++ predictor/src/prometheusQueries.py
@@ -0,0 +1,7 @@
+Queries = {
+    "mem" : "100 * (1 - ((avg_over_time(node_memory_MemFree[24h]) + avg_over_time(node_memory_Cached[24h]) + avg_over_time(node_memory_Buffers[24h])) / avg_over_time(node_memory_MemTotal[24h])))",
+
+    "cpu" : "100 - (avg by (instance) (irate(node_cpu{job='k8s-nodes',mode='idle'}[5m])) * 100)",
+
+    "cpunonidle" : "sum(rate(node_cpu{mode!='idle',mode!='iowait',mode!~'^(?:guest.*)$'}[5m])) BY (instance)"
+}
