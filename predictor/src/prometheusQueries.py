b0e1e161a59d66718ff3f6b5b81ff7cfc3d43c5c
vbongale <vabongal@ncsu.edu>
2019-03-24 17:37:46 -0400

    Add a prometheus query for IO metrics
    Query:
    node_disk_io_now{device='xvda'}
    device selected is 'xvda' which is the root mount point in the machine.

--- predictor/src/prometheusQueries.py
+++ predictor/src/prometheusQueries.py
@@ -3,5 +3,7 @@ Queries = {
 
     "cpu" : "100 - (avg by (instance) (irate(node_cpu{job='k8s-nodes',mode='idle'}[5m])) * 100)",
 
-    "cpunonidle" : "sum(rate(node_cpu{mode!='idle',mode!='iowait',mode!~'^(?:guest.*)$'}[5m])) BY (instance)"
+    "cpunonidle" : "sum(rate(node_cpu{mode!='idle',mode!='iowait',mode!~'^(?:guest.*)$'}[5m])) BY (instance)",
+
+    "diskio" : "node_disk_io_now{device='xvda'}"
 }
