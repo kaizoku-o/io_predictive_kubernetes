6cfa23ee88e4130ce24015ccf316e2225644a01a
Sohail <sashaikh@ncsu.edu>
2019-03-26 01:08:19 -0400

    Fixed indentation #49

--- predictor/src/PrometheusDataHandler.py
+++ predictor/src/PrometheusDataHandler.py
@@ -8,12 +8,12 @@ class PrometheusDataHandler(BaseDataHandler):
 
 	def get_data():
 		"""
-	    Fetch data from prometheus as required for training and prediction
-    	Arguments:
-        	No arguments
-	    Returns:
-    	    A dictionary of Node_IP -> list of tuples(time, workload)
-	    """		
+		Fetch data from prometheus as required for training and prediction
+		Arguments:
+			No arguments
+		Returns:
+			A dictionary of Node_IP -> list of tuples(time, workload)
+		"""		
 		result = run_query_range(Queries["cpu"], 1552290000, 1552320000, 60)
 		hosts = {x[0] for x in result}
 		response = {}
