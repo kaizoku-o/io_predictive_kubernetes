e7c590f339cb998c35b302b7af94b81ebbab2067
Sohail <sashaikh@ncsu.edu>
2019-03-29 08:39:17 -0400

    #49 Algorithm implementations
    Implemented:
    1. Linear support vector regressions,
    2. Gaussian svrs,
    3. Holt-Winter's DES,
    4. Weighted Moving Average
    5. Simple Exponential Smoothing
    
    Testing:
    Tested end to end on K8s prometheus node with postman.
    
    Issues:
    Linear svr gives a very off prediction. But this can be handled in #42
    
    Results:
    Expected Value:  (87.1333333333314)
    LinearReg  [73.63621658]
    ARIMA [72.5383048]
    linear svr  [8.82688509e+13]
    gaussian svr  [72.53055556]
    SES  [73.63599862]
    des  [79.93203613]

--- predictor/src/Predictor.py
+++ predictor/src/Predictor.py
@@ -1,5 +1,8 @@
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing
 import statsmodels.api as sm
+import numpy as np
+from sklearn.svm import SVR
 
 class Predictor:
 	model_list_ = []
@@ -22,20 +25,65 @@ class Predictor:
 
 		return 	prediction
 
-	def linear_svm(self, values):
+	def linear_svr(self, values, law=1):
+		Y_Train = [x[1] for x in values] # dependent varaible
+		X_Train = [[x[0]] for x in values] # independent variable
 
-		pass
+		model = SVR(kernel='linear', C=100, gamma='auto').fit(X_Train,
+					Y_Train)
 
-	def guassian_svm():
-		pass
+		sampling_duration = X_Train[1][0] - X_Train[0][0]
+		last_X_Train = X_Train[-1][0]
+
+		# Getting the times at which the next values are to be predicted
+		X_Pred = [[last_X_Train + x * sampling_duration] for x in range(law)]
+
+		predictions = model.predict(X_Pred)
+
+		return predictions
+
+	def guassian_svr(self, values, law=1):
+		Y_Train = [x[1] for x in values] # dependent varaible
+		X_Train = [[x[0]] for x in values] # independent variable
+
+		model = SVR(kernel='rbf', C=1, gamma='auto', epsilon=.1).fit(X_Train,
+					Y_Train)
+
+		sampling_duration = X_Train[1][0] - X_Train[0][0]
+		last_X_Train = X_Train[-1][0]
+
+		# Getting the times at which the next values are to be predicted
+		X_Pred = [[last_X_Train + x * sampling_duration] for x in range(law)]
+
+		predictions = model.predict(X_Pred)
+		
+		return predictions
+
+	def simple_exponential_smoothing(self, values, law=1):
+		Y_Train = [x[1] for x in values]
+		model = SimpleExpSmoothing(Y_Train).fit()
+		predictions = model.forecast(law)
+		return predictions
 
 	# double exponential smoothing
-	def des():
-		pass
+	def holtWinters_des(self, values, law=1):
+		Y_Train = [x[1] for x in values]
+		model = ExponentialSmoothing(Y_Train, seasonal_periods=20, 
+			trend='add', seasonal='add').fit(use_boxcox=True)
+		predictions = model.forecast(law)
+		return predictions
 
 	# weighted moving average
-	def wma():
-		pass
+	def wma(self, values, law=1):
+		Y_Train = np.array([x[1] for x in values])
+		prediction = []
+
+		for i in range(law):
+			weights = np.arange(1, len(Y_Train) + 1)
+			prediction.append(np.sum(Y_Train*weights)/sum(weights))
+			Y_Train = np.append(Y_Train, [prediction])
+		
+		return prediction
 		
 	def linear_regression(self, values, law=1):
 		Y_Train = [x[1] for x in values] # dependent varaible
@@ -54,8 +102,7 @@ class Predictor:
 		# Getting the times at which the next values are to be predicted
 		X_Pred = [last_X_Train + x * sampling_duration for x in range(law)]
 		predictions = model.predict(X_Pred)
-		print(X_Pred)
 		return predictions
 
 	def get_prediction(self, values, lookahead_window=1):
-		return self.arima(values, lookahead_window)
+		return self.holtWinters_des(values, lookahead_window)
