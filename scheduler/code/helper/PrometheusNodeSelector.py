a6b500898732858395513f272cd4723c50d484bd
kdmarti2 <kdmarti2@ncsu.edu>
2019-05-03 17:44:21 -0400

    Update PrometheusNodeSelector.py

--- scheduler/code/helper/PrometheusNodeSelector.py
+++ scheduler/code/helper/PrometheusNodeSelector.py
@@ -8,14 +8,29 @@ import random
 
 
 logging = logit.get_logger()
-
+##
+#Child class that inherits from GenericNodeSelector
+#Class is responsible for asking predicator for all future workloads
+#for all nodes in our system
+#Class will return the node with the lowest future workload of.
+##
 class PrometheusNodeSelector(GenericNodeSelector):
+    
+    ##
+    #String alg - expect value is cpu, mem, io.  Determine which workload type to query for
+    #String api - API location to query the predicator
+    #log_collector - GeneralStatsCollector object used for debug messages
+    ##
     def __init__(self,alg,api,log_collector=None):
         self.alg = alg;
         self.api = api;
         self.log_collector = log_collector;
         super().__init__()
 
+    ##
+    #nodeList -> a dictionary where the keys are the node IPaddresses and the values are the K8s names
+    #Return String -> node that has the lowest future workload that K8s can understand.
+    ##
     def node_selection(self,nodeList: dict) -> str:
         if len(nodeList.values()) == 0:
             raise RuntimeError("No Nodes To select From")
